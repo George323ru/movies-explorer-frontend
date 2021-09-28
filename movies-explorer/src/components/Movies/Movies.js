8ec9dd30a02de20c8f8279bfb1d1a5e3b293ed18
Georgiy Gusev <gus3225@yandex.ru>
2021-09-28 11:16:09 +0500

    feat: fill Movies.js

---
+++ movies-explorer/src/components/Movies/Movies.js
@@ -0,0 +1,13 @@
+import "./Movies.css";
+
+const Movies = () => {
+  return (
+    <section className="movies">
+      <div className="movies__container">
+
+      </div>
+    </section>
+  );
+};
+
+export default Movies;
