77ec283a158df0d0282a11e80c06a5a0a8894921
Georgiy Gusev <gus3225@yandex.ru>
2021-09-28 11:14:21 +0500

    feat: make search

---
+++ movies-explorer/src/components/Movies/SearchForm/SearchForm.js
@@ -0,0 +1,13 @@
+import "./SearchForm.css";
+
+const SearchForm = () => {
+  return (
+    <section className="search-form">
+      <div className="search-form__container">
+        <input className="search-form__input" placeholder="Фильм"></input>
+      </div>
+    </section>
+  );
+};
+
+export default SearchForm;
