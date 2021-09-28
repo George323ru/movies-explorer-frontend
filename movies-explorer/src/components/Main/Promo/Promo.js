dead266ca928ec110b7ec7b68718228879f2f719
Georgiy Gusev <gus3225@yandex.ru>
2021-09-28 11:10:55 +0500

    feat: make promo

---
+++ movies-explorer/src/components/Main/Promo/Promo.js
@@ -0,0 +1,23 @@
+import "./Promo.css";
+
+const Promo = () => {
+  return (
+    <section className="promo">
+      <div className="promo__container">
+        <div className="promo__wrapper">
+          <h1 className="promo__title">
+            Учебный проект студента факультета
+            Веб&#8209;разработки.
+          </h1>
+          <p className="promo__subtitle">
+            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+          </p>
+          <button className="promo__button">Узнать больше</button>
+        </div>
+        <img src="../../../images/logo (2).svg"></img>
+      </div>
+    </section>
+  );
+};
+
+export default Promo;
