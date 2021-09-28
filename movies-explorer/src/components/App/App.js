2dd742fc91d1f2d9dfa89212b68584cc0d3f9f70
Georgiy Gusev <gus3225@yandex.ru>
2021-09-28 11:07:47 +0500

    feat: make portfolio

--- movies-explorer/src/components/App/App.js
+++ movies-explorer/src/components/App/App.js
@@ -2,17 +2,24 @@ import { Route, Switch } from "react-router-dom";
 import React from "react";
 import './App.css';
 import Header from '../Header/Header';
+import Promo from "../Main/Promo/Promo";
+import Movies from "../Movies/Movies";
+import SearchForm from "../Movies/SearchForm/SearchForm";
 import AboutProject from '../Main/AboutProject/AboutProject';
 import Techs from "../Main/Techs/Techs";
 import AboutMe from "../Main/AboutMe/AboutMe";
 import Portfolio from "../Main/Portfolio/Portfolio"
 // import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
-import Footer from '../Footer/Footer'
+import Footer from '../Footer/Footer';
+
 
 const App = () => {
   return (
     <div className="page">
       <Header />
+      <Movies />
+      <SearchForm />
+      <Promo />
       <AboutProject />
       <Techs />
       <AboutMe />
