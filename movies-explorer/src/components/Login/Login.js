90f7e124403be36ed461762ffef8510da4cb942a
Georgiy Gusev <gus3225@yandex.ru>
2021-09-29 11:59:29 +0500

    feat: create variables in css

--- movies-explorer/src/components/Login/Login.js
+++ movies-explorer/src/components/Login/Login.js
@@ -1,9 +1,44 @@
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
   return (
-    <section className="login">
-      <div className="login__container">
+    <section className="authorization">
+      <div className="authorization__container">
+        <form className="authorization__form">
+          <h3 className="authorization__titile">Рады видеть!</h3>
+          <label className="authorization__label" for="email">E-mail</label>
+          <input
+            type='email'
+            required
+            minLength='2'
+            maxLength='200'
+            name='email'
+            className='authorization__input'
+            id="email"
+          />
+          <label className="authorization__label" for="password">Пароль</label>
+          <input
+            type='password'
+            required
+            minLength='2'
+            maxLength='200'
+            name='password'
+            className='authorization__input'
+            id="password"
+          />
+          <button className='authorization__button' type='submit'>
+            Войти
+          </button>
+          <p className='authorization__suggest'>
+            Ещё не зарегистрированы?
+            <Link className='authorization__link' to='/sign-in'>
+              Регистрация
+            </Link>
+          </p>
+
+        </form>
+
 
       </div>
     </section>
--- movies-explorer/src/components/Register/Register.js
+++ movies-explorer/src/components/Register/Register.js
@@ -3,46 +3,46 @@ import "./Register.css";
 
 const Register = () => {
   return (
-    <section className="register">
-      <div className="register__container">
-        <form className="register__form">
-          <h3 className="register__titile">Добро пожаловать!</h3>
-          <label className="register__label" for="name">Имя</label>
+    <section className="authorization">
+      <div className="authorization__container">
+        <form className="authorization__form">
+          <h3 className="authorization__titile">Добро пожаловать!</h3>
+          <label className="authorization__label" for="name">Имя</label>
           <input
             type='text'
             required
             minLength='2'
             maxLength='200'
             name='text'
-            className='register__input'
+            className='authorization__input'
             id="name"
           />
-          <label className="register__label" for="email">E-mail</label>
+          <label className="authorization__label" for="email">E-mail</label>
           <input
             type='email'
             required
             minLength='2'
             maxLength='200'
             name='email'
-            className='register__input'
+            className='authorization__input'
             id="email"
           />
-          <label className="register__label" for="password">Пароль</label>
+          <label className="authorization__label" for="password">Пароль</label>
           <input
             type='password'
             required
             minLength='2'
             maxLength='200'
             name='password'
-            className='register__input'
+            className='authorization__input'
             id="password"
           />
-          <button className='register__button' type='submit'>
+          <button className='authorization__button' type='submit'>
             Зарегистрироваться
           </button>
-          <p className='register__suggest'>
+          <p className='authorization__suggest'>
             Уже зарегистрированы?
-            <Link className='register__link' to='/sign-in'>
+            <Link className='authorization__link' to='/sign-in'>
               Войти
             </Link>
           </p>
