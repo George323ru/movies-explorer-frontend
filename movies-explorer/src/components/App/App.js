b73daef75b6817b3198e85e73ec7e6c8f01e748e
Georgiy Gusev <gus3225@yandex.ru>
2021-09-29 12:32:32 +0500

    feat: make the 404 page

--- movies-explorer/src/components/App/App.js
+++ movies-explorer/src/components/App/App.js
@@ -2,20 +2,12 @@ import { Route, Switch } from "react-router-dom";
 import React from "react";
 import './App.css';
 import Header from '../Header/Header';
-import Promo from "../Main/Promo/Promo";
 import Movies from "../Movies/Movies";
-import MoviesCard from "../Movies/MoviesCard/MoviesCard";
-import MoviesCardList from "../Movies/MoviesCardList/MoviesCardList";
 import Main from "../Main/Main";
-import SearchForm from "../Movies/SearchForm/SearchForm";
-import AboutProject from '../Main/AboutProject/AboutProject';
-import Techs from "../Main/Techs/Techs";
-import AboutMe from "../Main/AboutMe/AboutMe";
-import Portfolio from "../Main/Portfolio/Portfolio"
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import Footer from '../Footer/Footer';
 import Register from "../Register/Register";
 import Login from "../Login/Login";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
 
 
 const App = () => {
@@ -23,28 +15,22 @@ const App = () => {
     <div className="page">
       <Header />
       <Switch>
-        <ProtectedRoute
-          exact
-          path='/'
-          component={Main}
-        />
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
         <Route path='/sign-in'>
           <Login />
         </Route>
         <Route path='/sign-up'>
           <Register />
         </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
       </Switch>
-      <Movies />
-      <MoviesCard />
-      <MoviesCardList />
-      <SearchForm />
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-      <Portfolio />
-      { }
       <Footer />
     </div>
   );
--- movies-explorer/src/components/Movies/Movies.js
+++ movies-explorer/src/components/Movies/Movies.js
@@ -1,10 +1,13 @@
 import "./Movies.css";
+import SearchForm from "./SearchForm/SearchForm";
+import MoviesCardList from "./MoviesCardList/MoviesCardList";
 
 const Movies = () => {
   return (
     <section className="movies">
       <div className="movies__container">
-
+        <SearchForm />
+        <MoviesCardList />
       </div>
     </section>
   );
