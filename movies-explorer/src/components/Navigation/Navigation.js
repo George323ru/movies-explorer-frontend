71475cb9ee594f5af629a2f1fe1a3470593a4b9b
Georgiy Gusev <gus3225@yandex.ru>
2021-10-01 18:48:17 +0500

    feat: make switch links in the Navigation

--- movies-explorer/src/components/Navigation/Navigation.js
+++ movies-explorer/src/components/Navigation/Navigation.js
@@ -1,35 +1,56 @@
 import { Link, useLocation } from "react-router-dom";
 import "./Navigation.css";
+import "./_hidden/navigation_hidden.css"
+import accauntLogo from "../../images/icon-accaunt.svg";
 
 const Navigation = () => {
-
   const { pathname } = useLocation();
 
-  const accauntLinksHidden = `${pathname === "/"
-    ? "navigation_hidden"
-    : ""}`;
+  const accauntLinksHidden = `${pathname === "/" ? "navigation_hidden" : ""}`;
 
   const navLinksHidden = `${pathname === "/sign-in" || pathname === "/sign-up"
     ? "navigation_hidden"
-    : ""}`;
+    : ""
+    }`;
 
-  const authLinksHidden = `${pathname === "/movies" || pathname === "/saved-movies" || pathname === "/profile"
+  const authLinksHidden = `${pathname === "/movies" ||
+    pathname === "/saved-movies" ||
+    pathname === "/profile"
     ? "navigation_hidden"
-    : ""}`;
-
+    : ""
+    }`;
 
   return (
-    <nav className={`navigation ${navLinksHidden}`} >
-      <div className={`navigation__links ${authLinksHidden}`}>
-        <Link className="navigation__register-link" to="/sign-up">Регистрация</Link>
-        <Link className="navigation__login-link" to="/sign-in">Войти</Link>
-      </div>
-      <div className={`navigation__links ${accauntLinksHidden}`}>
-        <Link className="navigation__link" to="/movies">Фильмы</Link>
-        <Link className="navigation__link" to="/saved-movies">Сохранённые фильмы</Link>
-        <Link className="navigation__link" to="/profile">Аккаунт</Link>
-      </div>
-    </nav >
+    <nav className={`navigation ${navLinksHidden}`}>
+      <ul className={`navigation__links ${authLinksHidden}`}>
+        <li className="navigation__links-item">
+          <Link className="navigation__register-link" to="/sign-up">
+            Регистрация
+          </Link>
+        </li>
+        <li className="navigation__links-item">
+          <Link className="navigation__login-link" to="/sign-in">
+            Войти
+          </Link>
+        </li>
+      </ul>
+      <ul className={`navigation__links ${accauntLinksHidden}`}>
+        <li className="navigation__links-item">
+          <Link className="navigation__movies-link" to="/movies">
+            Фильмы
+          </Link>
+          <Link className="navigation__save-films-link" to="/saved-movies">
+            Сохранённые фильмы
+          </Link>
+        </li>
+        <li className="navigation__links-item">
+          <Link className="navigation__profile-link" to="/profile">
+            Аккаунт
+            <img className="navigation__accaunt-image" src={accauntLogo}></img>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
