0e42597753853e60b2761d13bd6a7b763162530c
Georgiy Gusev <gus3225@yandex.ru>
2021-10-02 10:35:23 +0500

    feat: make adaptiv for Main 1200px

--- movies-explorer/src/components/Main/AboutProject/AboutProject.js
+++ movies-explorer/src/components/Main/AboutProject/AboutProject.js
@@ -5,16 +5,16 @@ const AboutProject = () => {
     <section className="about-project">
       <div className="about-project__container">
         <h2 className="about-project__title">О проекте</h2>
-        <div className="about-project__description">
-          <div className="about-project__column">
+        <ul className="about-project__description">
+          <li className="about-project__column">
             <p className="about-project__subtitle">Дипломный проект включал 5 этапов</p>
             <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-          </div>
-          <div className="about-project__column">
+          </li>
+          <li className="about-project__column">
             <p className="about-project__subtitle">На выполнение диплома ушло 5 недель</p>
             <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-          </div>
-        </div>
+          </li>
+        </ul>
         <div className="about-project__timeline">
           <div className="about-project__timeline-backend">
             <p className="about-project__timeline-part-first">1 неделя</p>
