77e65aa889b1aa833de6bd3bc2a4ad22710088f0
Georgiy Gusev <gus3225@yandex.ru>
2021-10-10 16:59:55 +0500

    feat: make the number of cards depending on the size of the window

--- movies-explorer/src/components/Movies/MoviesCardList/MoviesCardList.js
+++ movies-explorer/src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,7 +1,33 @@
 import "./MoviesCardList.css";
 import Card from '../MoviesCard/MoviesCard'
+import { useState } from "react";
 
 const MoviesCardList = ({ movies }) => {
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  console.log(screenWidth)
+
+
+  let maxCardNumber
+
+  switch (true) {
+    case screenWidth > 1280:
+      maxCardNumber = 12
+      break;
+    case screenWidth < 1279 && screenWidth > 786:
+      maxCardNumber = 8
+      break;
+    case screenWidth > 320 && screenWidth < 786:
+      maxCardNumber = 5
+      break;
+
+    default:
+
+  }
+
+  const [cardsLimit, setCardsLimit] = useState(maxCardNumber);
+
+
 
   return (
     <section className="movies-card-list">
@@ -11,7 +37,7 @@ const MoviesCardList = ({ movies }) => {
             key={id}
             movie={movies}
           />
-        ))}
+        )).slice(0, cardsLimit)}
       </ul>
       <div className="movies-card-list__pagination">
         <button className="movies-card-list__append-button">
